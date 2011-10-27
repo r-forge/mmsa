fb64165019a8758f06ba448cccdac3dd24a55c7e
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2011-10-27 16:55:29 +0000

    added interface ideas.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Work/test-GreenGenes.R
+++ pkg/Work/test-GreenGenes.R
@@ -1,7 +1,23 @@
+library(MMSA)
+
 #create an object of class gencollection
 gc<-GenCollection()
 #read all fasta files from a directory
 gc<- read_Greengenes(gc,"greengenes/")
+
+## gc has sequences
+
+gc_nsv <- toNSV(gc)
+
+gc_model <- GenModel(gc_nsv)
+
+
+plot(gc_model)
+
+##subset
+
+gc_nsv_Thermo <- select(gc_nsv, level="class", name="Thermoplasmata")
+
 #count at a particular level
 count.GenCollection(gc,"kingdom")
 #list at a particular level
