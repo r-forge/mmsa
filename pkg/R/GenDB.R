2443af5e4214307cfe722964fe49af92a53318f8
andynagar (Andy Nagar) <andynagar@yahoo.com>
2011-12-13 06:12:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/GenDB.R
@@ -0,0 +1,161 @@
+openGenDB <- function(dbName, drv=NULL) {
+    if(!file.exists(dbName)) 
+	stop("GenDB does not exist. Use createGenDB first.\n")
+    
+    if(is.null(drv)) drv<-dbDriver("SQLite");
+
+    db<-dbConnect(drv, dbname = dbName);
+
+    db <- list(db=db, dbName=dbName)
+    class(db) <- "GenDB"
+    db
+}
+
+createGenDB <- function(dbName, classification=GenClass16S_Greengenes(),
+	type="sequence", annotation="", drv=NULL) {
+
+    if(file.exists(dbName)) 
+	stop("GenDB already exist. Use openGenDB.\n")
+    
+    
+    if(is.null(drv)) drv<-dbDriver("SQLite");
+    db<-dbConnect(drv, dbname = dbName);
+
+
+    #first table stores the Classification with org_name as PK
+    cl <- paste(paste("'",names(classification),"'", sep=''), "TEXT", 
+	    collapse=', ')
+    cl <- paste(cl, "PRIMARY KEY") ## the lowest rank is the primary key
+
+    #first table is called classification and stores the class hierarchy
+    try(
+	    dbSendQuery(db, 
+		    statement = paste("CREATE TABLE classification ( " ,
+			    cl, ")", sep=''))
+	    )
+
+    #second table stores the sequences as BLOB with org_name as PK
+    seq <- "org_name TEXT PRIMARY KEY REFERENCES classification(org_name), sequence BLOB "
+    try(
+	    dbSendQuery(db, 
+		    statement = paste("CREATE TABLE sequences (",
+			    seq,  ")", sep=''))
+	    )
+
+
+    #third table stores the meta data
+    meta<-"name TEXT, type TEXT, annotation TEXT"
+    try(
+	    dbSendQuery(db, 
+		    statement = paste("CREATE TABLE metaData (",
+			    meta,  ")", sep=''))
+	    )
+
+    #insert data into meta
+    try(
+
+	    dbSendQuery(db,          
+		    statement = paste("INSERT INTO metaData  VALUES ('sequences', 'sequence', '", annotation, "')", sep="")
+		    )
+	    )
+
+    db <- list(db=db, dbName=dbName)
+    class(db) <- "GenDB"
+    db
+}
+
+
+# CLOSE THE CONNECTION
+#dbDisconnect(con)
+#dbUnloadDriver(drv)
+
+closeGenDB <- function(db) {
+    dbDisconnect(db)
+}
+
+listGenDB <- function(db) {
+    dbListTables(db)
+}
+
+print.GenDB <- function(object) {
+    cat("Object of class GenDB")
+    cat(" with", nSequences(object), "sequences.\n")
+    cat("DB File:", object$dbName, "\n")
+    cat("Tables: ")
+    print(dbListTables(object$db))
+}
+
+
+### get classification info
+
+getClassification <- function(db) {
+    cl <- dbListFields(db$db, "classification")
+    cl <- head(cl, length(cl)-1L)   ### remove data
+    cl
+}
+
+nSequences <- function(db, rank=NULL, name=NULL) {
+    dbGetQuery(db$db, 
+	    statement = paste("SELECT COUNT(*) FROM classification", 
+		    .getWhere(db, rank, name)))[1,1]
+
+}
+
+getRank <- function(db, rank=NULL, whereRank=NULL, whereName=NULL) {
+    fields <- getClassification(db)
+    cols <- paste("[", fields[.pmatchRank(db, rank, 
+		    numeric=TRUE)],"]", sep='')
+    dbGetQuery(db$db, 
+	    statement = paste("SELECT DISTINCT ",cols,
+		    " FROM classification ", 
+		    .getWhere(db, whereRank, whereName)))
+}
+
+getSequences <- function(db,  rank=NULL, name=NULL, table="sequences", limit=-1) {
+
+    if(limit<0) limit <- "" 
+    else limit <- paste(" LIMIT",limit)
+	if (table =="sequences")
+	{
+		dbGetQuery(db$db, 
+			statement = paste("SELECT sequence FROM ", table ,
+				" INNER JOIN classification ON classification.org_name = ",
+				table, ".org_name ", 
+				.getWhere(db, rank, name), limit)
+			)
+	}
+	else
+	{
+		NSV<-dbGetQuery(db$db, 
+			statement = paste("SELECT NSV FROM ", table ,
+				" INNER JOIN classification ON classification.org_name = ",
+				table, ".org_name ", 
+				.getWhere(db, rank, name), limit)
+			)
+		NSV<-lapply(NSV,decodeSequence)		
+	}
+    ### FIXME: check in metadata table if we have to decode (e.g., if it is NSV)
+}
+
+
+### helper
+
+.pmatchRank <- function(col, rank, numeric=FALSE) {
+    fields <- dbListFields(col$db, "classification")
+    if(is.null(rank)) m <- 1
+    else {
+	m <- pmatch(tolower(rank), tolower(fields))
+	if(any(is.na(m))) stop("Rank not found!")
+    }
+
+    if(numeric) m
+    else fields[m]
+    }
+
+.getWhere <- function(col, rank, name) {
+    if(is.null(rank) && is.null(name)) where <- ""
+    else where <- paste("WHERE classification.", .pmatchRank(col, rank), 
+		" LIKE '", name,"%'", sep='')
+    where
+}
+
