2b4b88211fa9df1b3915864d53cd6aee60dbec8c
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2012-03-13 21:13:30 +0000

    fixed formating
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/GenModel.R
+++ pkg/R/GenModel.R
@@ -100,30 +100,35 @@ scoreSequence.GenModel <- function(x, newdata, method = c("prod", "malik",
     # for Kullback method add 1
     if(x$model@measure=="Kullback") newdata <- newdata+1
 
-	    method <- match.arg(method)
-            
-			if(method == "prod" || is.na(method) ||
-				length(method)==0) { score(x$model,
-				newdata=newdata, method="prod",
-				match_cluster=match_cluster, plus_one=plus_one,
-				initial_transition=initial_transition) }
+    method <- match.arg(method)
 
-	    if(method == "malik") { emm_test<-build(emm_test,newdata)
-	    distance<-0
-	    d<-dist(cluster_centers(emm_test),cluster_centers(x$model))
-	    closestClusters<-apply(d, MARGIN=1, FUN=which.min) for(i in
-		    1:length(closestClusters)) { closestDist <-
-	    d[i,closestClusters[i]] #check if previous transition exists in
-	    model
-	    previousTransition<-transition(x$model,as.character(closestClusters[i]),as.character(closestClusters[i]-1))
-	    if (previousTransition > 0) previousTransition=1 else if
-	    (previousTransition ==0) previousTransition =
-	    -log(1/nstates(emm_test));
-						
-						distance<-distance+(closestDist*previousTransition)
-					    } #for each state in emm_test find
-					    closest cluster #for(i in
-					    1:nstates(emm_test)) #{
+    if(method == "prod" 
+	    || is.na(method) 
+	    || length(method)==0) { 
+	score(x$model, newdata=newdata, method="prod",
+		match_cluster=match_cluster, plus_one=plus_one,
+		initial_transition=initial_transition) 
+    }
+
+    if(method == "malik") { 
+	emm_test<-build(emm_test,newdata)
+	distance<-0
+	d<-dist(cluster_centers(emm_test),cluster_centers(x$model))
+	
+	closestClusters<-apply(d, MARGIN=1, FUN=which.min) 
+	for(i in 1:length(closestClusters)) { 
+	    closestDist <- d[i,closestClusters[i]] #check if previous transition exists in model
+	    previousTransition <- transition(x$model,
+		    as.character(closestClusters[i]),
+		    as.character(closestClusters[i]-1))
+    
+	    if (previousTransition > 0) previousTransition <- 1 
+	    else if (previousTransition ==0) previousTransition <- -log(1/nstates(emm_test))
+
+		distance<-distance+(closestDist*previousTransition)
+	} #for each state in emm_test find closest cluster 
+	#for(i in 1:nstates(emm_test)) 
+	#{
 				#	d<-numeric(nstates(x$model)) for(j in
 				    #	1:nstates(x$model)) {
 				    #	d[j]<-dist(rbind(cluster_centers(emm_test)[i,],cluster_centers(x$model)[j,]),method="Manhattan")
@@ -139,21 +144,23 @@ scoreSequence.GenModel <- function(x, newdata, method = c("prod", "malik",
 				    #		-log(1/nstates(emm_test));
 				#		
 				#		distance<-distance+(closestDist*previousTransition)
-				    #		}
-				#} return (1/(1+distance))
-				
-	    }
+	#		}
+	#} return (1/(1+distance))
 
-	    if(method == "misstran") { transitionTable <-
-	    transition_table(x$model, newdata, method="prob", match_cluster,
-		    plus_one, initial_transition) missingTransitions <-
-	    sum(transitionTable[,3]==0) #missingTransitions <-
-	    length(whichi(is.na(transitionTable[,1]) ||
-			    which(is.na(transitionTable[,2]))))
-	    return(missingTransitions)     }
+    }
+
+    if(method == "misstran") { 
+	transitionTable <- transition_table(x$model, 
+		newdata, method="prob", match_cluster,
+		plus_one, initial_transition) 
+	missingTransitions <- sum(transitionTable[,3]==0) 
+	#missingTransitions <- length(whichi(is.na(transitionTable[,1]) ||
+	##	    which(is.na(transitionTable[,2]))))
+	return(missingTransitions)    
+    }
 
 
-  }
+}
 
 
 prune.GenModel <- function(x, ...) {
