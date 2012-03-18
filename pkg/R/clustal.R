26a98d1361a76be3482b0ea535cd85999e39778f
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2012-03-18 20:49:53 +0000

    added simrank and clustal.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clustal.R
+++ pkg/R/clustal.R
@@ -1,26 +1,16 @@
-library(Biostrings)
-
-## install clustalw (is a Ubuntu package) 
-
-## example
-#filepath <- "test2_org.FASTA"
-#s <- read.DNAStringSet(filepath, format="fasta",
-#	nrec=-1L, skip=0L, use.names=TRUE)
-#al <- clustal(s)
-#al
 
 
 clustal <- function(x) {
     ## get temp files and change working directory
     wd <- tempdir()
     dir <- getwd()
+    temp_file <- basename(tempfile(tmpdir = wd))
     
-    on.exit(setwd(dir))
+    on.exit({file.remove(temp_file); setwd(dir)})
     setwd(wd)
 
-    temp_file <- basename(tempfile(tmpdir = wd))
 
-    write.XStringSet(s, temp_file, append=FALSE, format="fasta")
+    write.XStringSet(x, temp_file, append=FALSE, format="fasta")
 
     ## call clustalw (needs to be installed and in the path!)
     system(paste("clustalw", temp_file))
--- pkg/R/simRank.R
+++ pkg/R/simRank.R
@@ -3,17 +3,16 @@
 ### search tool, BMC Ecology 2011, 11:11
 
 ### x and y are vectors of sequences
-simRank <- function(x, y, k=3, alphabet = "acgt") {
-    x.kmers <- sapply(x, FUN=function(s) 
-	    count(s2c(s), word=k, alphabet=s2c(alphabet))) >0
-    y.kmers <- sapply(y, FUN=function(s) 
-	    count(s2c(s), word=k, alphabet=s2c(alphabet))) >0
+simRank <- function(x, k=7) {
+    x <- DNAStringSet(x)
+    
+    x.kmer <- oligonucleotideFrequency(x, k) >0
 
-    shared <- crossprod(x.kmers, y.kmers)
-    min <- outer(colSums(x.kmers), colSums(y.kmers), function(x, y) { 
-		sapply(1:length(x), FUN = function(i) min(x[i], y[i]))
-	    })
+    shared <- tcrossprod(x.kmer)
+    sum.kmer <- rowSums(x.kmer)
+    
+    min <- outer(sum.kmer, sum.kmer, function(x, y) pmin(x,y)) 
 
-    shared/min
+    as.simil(shared/min)
 }
 
