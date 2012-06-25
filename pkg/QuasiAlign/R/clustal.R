ad090460a9707327ba2d59ad27a6aea80513dcfb
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2012-06-25 16:10:18 +0000

    moved QuasiAlign a level down.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@202 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 9d92671fbe734009b1e7ec7e98e8c3cd94497675

---
+++ pkg/QuasiAlign/R/clustal.R
@@ -0,0 +1,63 @@
+clustal <- function(x, param=NULL) {
+    
+    ## get temp files and change working directory
+    wd <- tempdir()
+    dir <- getwd()
+    temp_file <- basename(tempfile(tmpdir = wd))
+    on.exit({
+		file.remove(Sys.glob(paste(temp_file, ".*", sep=""))) 
+		setwd(dir)
+	    })
+    setwd(wd)
+
+    infile <- paste(temp_file, ".in", sep="")
+    outfile <- paste(temp_file, ".aln", sep="")
+    
+    write.XStringSet(x, infile, append=FALSE, format="fasta")
+
+    ## call clustalw (needs to be installed and in the path!)
+    system(paste(Sys.which("clustalw -infile"), infile, param))
+
+    read.DNAMultipleAlignment(outfile, format="clustal")
+}
+
+clustal_help <- function() {
+    system(paste(Sys.which("clustalw"), "-help"))
+}
+
+
+clustal_profile <- function(x, y, param=NULL) {
+    
+    if(is(y, "DNAMultipleAlignment")) {
+	profileprofile <- TRUE
+	param2 <- "-profile"
+    } else {
+	profileprofile <- FALSE
+	param2 <- "-sequence"
+    }
+
+    ## get temp files and change working directory
+    wd <- tempdir()
+    dir <- getwd()
+    temp_file <- basename(tempfile(tmpdir = wd))
+    on.exit({
+		file.remove(Sys.glob(paste(temp_file, ".*", sep=""))) 
+		setwd(dir)
+	    })
+    setwd(wd)
+
+    prof1 <- paste(temp_file, ".in1", sep="")
+    prof2 <- paste(temp_file, ".in2", sep="")
+    outfile <- paste(temp_file, ".aln", sep="")
+    
+    write.phylip(x, prof1)
+    if(profileprofile) write.phylip(y, prof2)
+    else write.XStringSet(y, prof2, append=FALSE, format="fasta")
+    
+    ## call clustalw (needs to be installed and in the path!)
+    system(paste(Sys.which("clustalw -profile1="), prof1,
+		    "-profile2=", prof2, " ", param2, " ", param, sep=""))
+
+    read.DNAMultipleAlignment(paste(temp_file, ".aln", sep=""), 
+	    format="clustal")
+}
