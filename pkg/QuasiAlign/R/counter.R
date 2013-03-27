dc60597d57218a09f57dfbb42d846aa3ea7f45d1
andynagar (Andy Nagar) <andynagar@yahoo.com>
2013-03-27 15:54:24 +0000

    Added method to create offset NSV in counter file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@292 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 62fbc297c8d10fc8282425e31dc3ef678bc785ae

--- pkg/QuasiAlign/R/counter.R
+++ pkg/QuasiAlign/R/counter.R
@@ -2,27 +2,18 @@
 .counter <- function(x, window=100, overlap=0, word=3, 
 	last_window=FALSE, allOffsets=FALSE) {
     
-    #returns the sequence as a vector  
     x <- DNAString(x)
+    if (!allOffsets)
+	{
+	#returns the sequence as a vector  
 
     ret <- matrix(NA, ncol=4^word, nrow=0)
     
     if(length(x) < window) {
-	warning("Sequence is shorter than window size!")
-	return(matrix(nrow=0, ncol=0))
-    }
-
-    if (allOffsets)
-	end <- window
-    else
-	end <- 1
-
-    for(seqOffset in 1:end)
-    {   
-	y <- DNAString(x, start=seqOffset)    
-	if (length(y) < window) break;
-	l <- as.integer(length(y)/(window-overlap)) -1L
-
+		warning("Sequence is shorter than window size!")
+		return(matrix(nrow=0, ncol=0))
+   		}
+	l <- as.integer(length(x)/(window-overlap)) -1L
 	#start and end positions as vectors
 	start <- (window-overlap)*(0:l) + 1
 	end <- start + window - 1
@@ -35,16 +26,72 @@
 	}
 
 	mat <- t(sapply(1:length(start), FUN=function(i) 
-			oligonucleotideFrequency(DNAString(y,
+			oligonucleotideFrequency(DNAString(x,
 					start=start[i],
 					nchar=end[i]-start[i]+1), 
 				word)))
-	
-	if (seqOffset>1) ret <- rbind(ret, matrix(NA,nrow=1,ncol=4^word))
-	ret <- rbind(ret, mat)  
-    }
-    ret
+	ret <- rbind(ret,mat)
+	}	
+	else if (allOffsets)
+	{
+		freq<-oligonucleotideFrequency(DNAString(x,start=1,nchar=window),word)
+		fmatrix <- matrix(nrow=(length(x)-window+1),ncol=4^word)
+		fmatrix[1,]<-as.matrix(t(freq))
+		colnames(fmatrix) <- colnames(as.matrix(t(freq)))
+		start<-2
+		end <- window+1
+		while(end <=length(x))
+		{
+				next_mer<-DNAString(x,start=end-(word-1),nchar=word)
+				first_mer<-DNAString(x,start=start-1,nchar=word)
+				#update
+				freq[as.character(first_mer)]<-freq[as.character(first_mer)]-1
+				freq[as.character(next_mer)]<-freq[as.character(next_mer)]+1
+				#f<-rbind(f,t(freq)[,1:(4^word)])
+				fmatrix[start,]<-as.matrix(t(freq)[,1:(4^word)])
+				start<-start+1
+				end<-end+1
+		}
+		rearrangedIdx<-unlist(lapply(1:window,FUN=function(j){c(seq(j, length(x)-window+1, by=100),NA)}))
+		ret <- fmatrix[rearrangedIdx,]	
+	}
+	ret
+
 }
+
+#   if (allOffsets)
+#	end <- window
+#    else
+#	end <- 1
+#
+##    for(seqOffset in 1:end)
+#    {   
+#	y <- DNAString(x, start=seqOffset)    
+#	if (length(y) < window) break;
+#	l <- as.integer(length(y)/(window-overlap)) -1L
+#
+#	#start and end positions as vectors
+#	start <- (window-overlap)*(0:l) + 1
+#	end <- start + window - 1
+#
+#	if(last_window) {
+#	    if(tail(end,1) < length(x))	{
+#		start <- c(start, tail(end,1)+1)
+#		end <- c(end, length(x))
+#	    }
+#	}
+#
+#	mat <- t(sapply(1:length(start), FUN=function(i) 
+#			oligonucleotideFrequency(DNAString(y,
+#					start=start[i],
+#					nchar=end[i]-start[i]+1), 
+#				word)))
+#	
+#	if (seqOffset>1) ret <- rbind(ret, matrix(NA,nrow=1,ncol=4^word))
+#	ret <- rbind(ret, mat)  
+#    }
+#    ret
+#}
 
 #end
 
