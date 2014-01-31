8b08b62d59f6c5bb74823cc1f16bc106ed2ffddb
hahsler (Michael Hahsler) <mhahsler@lyle.smu.edu>
2014-01-31 23:06:05 +0000

    Fixed file location problem in boxshade.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mmsa@352 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d8e5420b5518438b1f6c8338a62c78e6554c7b7b

--- pkg/BioTools/R/boxshade.R
+++ pkg/BioTools/R/boxshade.R
@@ -18,53 +18,50 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 boxshade <- function(x, file, dev="pdf", param="-thr=0.5 -cons -def",
-	pdfCrop=TRUE) {
-    devs <- c(ps=1, eps=2, hpgl=3, rtf=4, crt=5, ansi=6, vt=7,
-	    ascii="b", fig="c", pict="d", html="e")
-
-    dev <- tolower(dev)
-    
-    ## pdf?
-    if(dev == "pdf") {
-	pdf <- TRUE
-	if(pdfCrop) dev <- "eps"
-	else dev <- "ps"
-    } else pdf <- FALSE
-    
-    if(dev %in% names(devs)) dev <- devs[[dev]]
-
-    ## get temp files and change working directory
-    wd <- tempdir()
-    dir <- getwd()
-    temp_file <- tempfile(tmpdir = wd)
-    on.exit({
-		file.remove(Sys.glob(paste(temp_file, ".*", sep=""))) 
-		setwd(dir) 
-    })
-
-    setwd(wd)
-    infile <- paste(temp_file, ".PHY", sep="")
-    write.phylip(x, filepath=infile)
-
-    ## call boxshade (needs to be installed and in the path!)
-    system(paste(.findExecutable(c("boxshade", "box")), 
-		    " -in=", infile, " -type=5",
-		    " -out=", file, " -dev=", dev, 
-		    param, sep=""))
-
-    if(pdf) {
-	file.rename(file, temp_file)
-	system(paste(.findExecutable(c("ps2pdf")), 
-			if(pdfCrop) "-dEPSCrop" else "", 
-			temp_file, file))  
-	unlink(temp_file)
-    }
-
+  pdfCrop=TRUE) {
+  devs <- c(ps=1, eps=2, hpgl=3, rtf=4, crt=5, ansi=6, vt=7,
+    ascii="b", fig="c", pict="d", html="e")
+  
+  dev <- tolower(dev)
+  
+  ## pdf?
+  if(dev == "pdf") {
+    pdf <- TRUE
+    if(pdfCrop) dev <- "eps"
+    else dev <- "ps"
+  } else pdf <- FALSE
+  
+  if(dev %in% names(devs)) dev <- devs[[dev]]
+  
+  ## get temp files and change working directory
+  wd <- tempdir()
+  temp_file <- tempfile(tmpdir = wd)
+  on.exit({
+    file.remove(Sys.glob(paste(temp_file, ".*", sep=""))) 
+  })
+  
+  infile <- paste(temp_file, ".PHY", sep="")
+  write.phylip(x, filepath=infile)
+  
+  ## call boxshade (needs to be installed and in the path!)
+  system(paste(.findExecutable(c("boxshade", "box")), 
+    " -in=", infile, " -type=5",
+    " -out=", file, " -dev=", dev, 
+    param, sep=""))
+  
+  if(pdf) {
+    file.rename(file, temp_file)
+    system(paste(.findExecutable(c("ps2pdf")), 
+      if(pdfCrop) "-dEPSCrop" else "", 
+      temp_file, file))  
+    unlink(temp_file)
+  }
+  
 }
 
 boxshade_help <- function() {
-    system(paste(.findExecutable(c("boxshade", "box")), 
-		    "-help"))
+  system(paste(.findExecutable(c("boxshade", "box")), 
+    "-help"))
 }
 
 
